4abf10d58790481754e129c91dbdb2badda6b201
ResYejin23 <recoding321@gmail.com>
2023-12-07 21:19:28 +0900

    portfolio Succ

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -4,7 +4,7 @@ const siteText = [
   {
     text: ["make", "site compliant with", "webstandard"],
     title: "비트를 이용한 사이트 제작",
-    code: "https://github.com/webstoryboy/port2023-vite",
+    code: "https://github.com/ResYejin23/port2023-vite",
     view: "https://port2023-vite.netlify.app",
     info: [
       "site coding",
@@ -15,7 +15,7 @@ const siteText = [
   {
     text: ["make", "site compliant with", "react.js"],
     title: "리액트를 이용한 사이트 제작",
-    code: "https://github.com/webstoryboy/port2023-react",
+    code: "https://github.com/ResYejin23/port2023-react",
     view: "https://port2023-react.netlify.app",
     info: [
       "site coding",
@@ -26,7 +26,7 @@ const siteText = [
   {
     text: ["make", "site compliant with", "vue.js"],
     title: "뷰를 이용한 사이트 제작",
-    code: "https://github.com/webstoryboy/port2023-vue",
+    code: "https://github.com/ResYejin23/port2023-vue",
     view: "https://port2023-vue.netlify.app",
     info: [
       "site coding",
@@ -37,7 +37,7 @@ const siteText = [
   {
     text: ["make", "site compliant with", "next.js"],
     title: "넥스트를 이용한 사이트 제작",
-    code: "https://github.com/webstoryboy/port2023-next",
+    code: "https://github.com/ResYejin23/port2023-next",
     view: "https://port2023-next.netlify.app",
     info: [
       "site coding",
